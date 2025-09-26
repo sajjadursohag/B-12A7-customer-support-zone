adeae1ebb215d8cb1f191528e2016a5294253763
sajjadursohag <sajjadursub07@gmail.com>
2025-09-26 16:31:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <nav>Navbar</nav>
+      <h2>Main Section</h2>
+      <footer>footer</footer>
+    </div>
+  )
+}
+
+export default App
